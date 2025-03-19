dcb05f3005136c52ea6ef3b3350c58d38513d39b
Judiraal <judiraal@judiraal.com>
2025-03-19 23:48:51 +0100

    fix mekanism update; fix performance issue between iron's spells, xaero and curios

---
+++ src/main/java/com/judiraal/moresweatcompat/mixin/irons_spellbooks/InvisibilityRingMixin.java
@@ -0,0 +1,31 @@
+package com.judiraal.moresweatcompat.mixin.irons_spellbooks;
+
+import io.redspace.ironsspellbooks.item.curios.InvisibiltyRing;
+import io.redspace.ironsspellbooks.item.curios.SimpleDescriptiveCurio;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.entity.LivingEntity;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+
+@Mixin(InvisibiltyRing.class)
+public abstract class InvisibilityRingMixin extends SimpleDescriptiveCurio {
+    public InvisibilityRingMixin(Properties properties) {
+        super(properties);
+    }
+
+    @Unique
+    private long clientLastGameTime;
+    private boolean playerIsEquipped;
+
+    @Override
+    public boolean isEquippedBy(@Nullable LivingEntity entity) {
+        if (entity instanceof LocalPlayer) {
+            long currentGameTime = entity.level().getGameTime();
+            if (currentGameTime == clientLastGameTime) return playerIsEquipped;
+            clientLastGameTime = currentGameTime;
+            return playerIsEquipped = super.isEquippedBy(entity);
+        }
+        return super.isEquippedBy(entity);
+    }
+}
